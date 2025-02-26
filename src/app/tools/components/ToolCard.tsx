3db3f626d8670adbaca747d897029df9b3f2931c
Ouk Outdam <outdam.ouk103@gmail.com>
2025-02-27 01:31:28 +0900

    feat: Add link property to ToolCard and wrap in anchor tag

--- src/app/tools/components/ToolCard.tsx
+++ src/app/tools/components/ToolCard.tsx
@@ -4,15 +4,18 @@ export type ToolCardProps = {
     name: string;
     description: string;
     status: 'to-do' | 'in-progress' | 'brushing-up' | 'completed';
+    link: string;
 }
 
 function ToolCard({tool} : {tool : ToolCardProps}) {
   return (
-        <div className="mt-2 border border-neutral-200 dark:border-neutral-700 p-4 rounded-lg">
-            <h2 className="text-lg font-bold">{tool.name}</h2>
-            <p>{tool.description}</p>
-            <p><strong>Status:</strong> {tool.status}</p>
-        </div>
+        <a href={tool.link} target='_blank'>
+            <div className="mt-3 border hover:bg-neutral-100 border-neutral-200 dark:border-neutral-700 dark:hover:bg-neutral-600 p-4 rounded-lg">
+                <h2 className="text-lg font-bold">{tool.name}</h2>
+                <p>{tool.description}</p>
+                <p><strong>Status:</strong> {tool.status}</p>
+            </div>
+        </a>
   )
 }
 
--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -4,12 +4,14 @@ const myTools: ToolCardProps[] = [
     {
         name: 'Speaking With ChatGPT',
         description: "A desktop application made with PyQT5 that uses OpenAI's api and Google's Text-to-Speech api to create a simple chatbot that you can talk to. It is a small project that I made for Toyohashi University of Technology's Summer Tech Camp 2024.",
-        status: 'completed'
+        status: 'completed',
+        link: "https://github.com/odakazu66/Summer-TECH-CAMP2024",
     },
     {
         name: 'Yoyaku Janken',
         description: "A simple rock-paper-scissors game that I made for my friends to use when everyone is busy. It is a web app where people can register their move and finally see who wins after everyone has made their choice.",
-        status: 'brushing-up'
+        status: 'brushing-up',
+        link: "https://github.com/oukoutdam/yoyaku_janken",
     },
 ]
 
