9aff07bc6eff97fe6e5df2721efe64434dd1a67e
Ouk Outdam <outdam.ouk103@gmail.com>
2025-06-23 03:15:20 +0900

    Fix Paths for Github Pages Deployment

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+
+  // fix paths for github pages
+  basePath: process.env.NODE_ENV === 'production' ? '/personal-homepage/' : '',
+  assetPrefix:
+    process.env.NODE_ENV === 'production' ? '/personal-homepage/' : '',
 };
 
 export default nextConfig;
