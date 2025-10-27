05ba02faaf1a3696dcefb9103a39174b973c450c
Ouk Outdam <outdam.ouk103@gmail.com>
2025-10-28 01:39:45 +0900

    Change Settings and Clean up to Move to Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: { unoptimized: true },
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-
-  // fix paths for github pages
-  basePath: process.env.PAGES_BASE_PATH,
-  assetPrefix: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
